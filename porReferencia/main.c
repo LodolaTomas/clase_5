390019a2a994709590f20726fb4ed8d1c260ee43
alumno <alumno>
2019-09-03 10:52:52 -0300

    punteros2

--- porReferencia/main.c
+++ porReferencia/main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void funcion(int*);
+
 int main()
 {
-    printf("Hello world!\n");
+    int numero=4;
+    printf("El numero  antes  de modificar es: %d\n",numero);
+    funcion(&numero);
+    printf("El numero despues de modificar es: %d",numero);
     return 0;
 }
+
+void funcion(int* numero)
+{
+     //*numero=100;///* operador de inacceso
+     printf("ingrese un numero: ");
+     scanf("%d", numero);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
